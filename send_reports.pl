b64493e1d6b503ba7881147cf03f84d702a565d9
Alceu Rodrigues de Freitas Junior <arfreitas@ragadaz.spo.virtua.com.br>
2016-09-11 22:15:23 -0300

    removed harcodes from send_reports.pl now send_reports.pl can select to send only PASSed reports adding a new distro pref

--- send_reports.pl
+++ send_reports.pl
@@ -1,41 +1,76 @@
 #!/usr/bin/perl
+use 5.012;
 use warnings;
 use strict;
 use Test::Reporter;
 use Test::Reporter::Transport::Socket;
 use Term::ProgressBar;
+use Getopt::Std;
+use File::Spec;
+
+my %opts;
+getopts('pr:h:', \%opts);
+
+die 'the parameter -r is required' unless (exists($opts{r}));
+die 'the parameter -h is required' unless (exists($opts{h}));
 
 my $sender = Test::Reporter->new(
     transport => 'Socket',
     transport_args => [
-        host => '192.168.1.118',
+        host => $opts{h},
         port => 8080 
     ],
 );
-my $dir = '/home/arfreitas/smoker';
-opendir(DIR,$dir) or die "cannot read $dir: $!";
-my @files = readdir(DIR);
-shift(@files);
-shift(@files);
-close(DIR);
-chdir($dir);
-
-my $progress = Term::ProgressBar->new( { count => scalar(@files) } );
 my $sent_counter = 0;
+my $files_ref = read_reports(\%opts);
+my $progress = Term::ProgressBar->new( { count => scalar( @{$files_ref} ) });
 
-foreach my $report(@files) {
+foreach my $report(@{$files_ref}) {
+
     if (-z $report) {
         warn "report $report has zero bytes lenght, skipping...\n";
         next;
     }
+
+    $progress->update( $sent_counter ) if ( submit( $sender, $report ) );
+}
+
+#$progress->update( scalar(@files) );
+
+sub submit {
+    my ($sender,$report) = @_;
+
     if ( $sender->read($report)->send ) {
         $sent_counter++;
 	unlink $report or warn "failed to remove $report: $!";
+        return 1;
     } else {
 	die $sender->errstr();
     }
-    $progress->update( $sent_counter );
+
 }
 
-$progress->update( scalar(@files) );
+sub read_reports {
+    my ($opts_ref) = @_;
+    my $dir = $opts_ref->{r};
+    opendir(my $input,$dir) or die "cannot read $dir: $!";
+    my $passed_regex = qr/^pass/;
+    my @files;
+   
+    if ( exists($opts_ref->{p}) ) {
+        print "Sending only reports that passed...\n";
+        while(readdir($input)) {
+            push(@files, File::Spec->catfile($dir,$_)) if ( $_ =~ $passed_regex );
+        }
+    } else {
+        while(readdir($input)) {
+            push(@files, File::Spec->catfile($dir,$_));
+        }
+        # removing "dot" files
+        shift(@files);
+        shift(@files);
+    }
 
+    close($input);
+    return \@files;
+}
