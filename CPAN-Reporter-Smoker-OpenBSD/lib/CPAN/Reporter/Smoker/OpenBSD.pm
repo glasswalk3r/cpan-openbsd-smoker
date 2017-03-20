c6e2dc714989d01cff1f644b7877e9bd44e043e6
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2017-03-20 18:37:39 -0300

    Added minimum Pod

--- CPAN-Reporter-Smoker-OpenBSD/lib/CPAN/Reporter/Smoker/OpenBSD.pm
+++ CPAN-Reporter-Smoker-OpenBSD/lib/CPAN/Reporter/Smoker/OpenBSD.pm
@@ -1,5 +1,50 @@
-use strict;
-use warnings;
 package CPAN::Reporter::Smoker::OpenBSD;
+
+=pod
+
+=head1 NAME
+
+CPAN::Reporter::Smoker::OpenBSD - set of scripts to manage a CPAN::Reporter::Smoker on OpenBSD
+
+=head1 DESCRIPTION
+
+This module is only Pod: you want to take a look at the programs C<block> and C<send_reports> documentation:
+
+=over
+
+=item *
+
+C<perldoc send_reports>
+
+=item *
+
+C<perldoc block>
+
+=back
+
+=head1 AUTHOR
+
+Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2017 of Alceu Rodrigues de Freitas Junior, arfreitas@cpan.org
+
+This file is part of CPAN OpenBSD Smoker.
+
+CPAN OpenBSD Smoker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CPAN OpenBSD Smoker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CPAN OpenBSD Smoker.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
 1;
