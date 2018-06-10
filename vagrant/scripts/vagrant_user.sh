3923a5f7cd33eacf1f3c8617b08f73869330af69
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2018-06-10 20:36:48 -0300

    Moved perl initialization to bootstrap
    All modules installed by this script were moved to the
    vagrant_boostrap.sh.

--- vagrant/scripts/vagrant_user.sh
+++ vagrant/scripts/vagrant_user.sh
@@ -6,7 +6,7 @@ idempotent_control='/home/vagrant/.vagrant_provision'
 
 # requirements for vagrant user:
 # - perlbrew
-# - basic modules: YAML::XS CPAN::SQLite Module::Version Log::Log4perl 
+# - basic modules: YAML::XS CPAN::SQLite Module::Version Log::Log4perl
 # - required: CPAN::Mini CPAN::Mini::LatestDistVersion
 # - cpanm --force POE::Component::SSLify -n (required, but broken)
 # - The last modules (depend on the above): POE::Component::Metabase::Client::Submit POE::Component::Metabase::Relay::Server metabase::relayd CPAN::Reporter::Smoker::OpenBSD
@@ -43,10 +43,17 @@ echo "Starting vagrant configuration at ${now}"
 if ! [ -f "${idempotent_control}" ]
 then
     echo "Configuring vagrant user"
-    git clone https://github.com/glasswalk3r/cpan-openbsd-smoker.git
+
+    if [ -d /home/vagrant/cpan-openbsd-smoker ]
+    then
+        cd /home/vagrant/cpan-openbsd-smoker
+        git pull
+    else
+        git clone https://github.com/glasswalk3r/cpan-openbsd-smoker.git
+    fi
 
     if [ $? -ne 0 ]
-    then 
+    then
         echo "Failed to execute git, aborting..."
         exit 1
     fi
@@ -54,11 +61,6 @@ then
     config_metabase
     echo 'Installing required Perl modules...'
     (echo "o conf urllist ${CPAN_MIRROR}"; echo 'o conf commit') | cpan
-    # using cpan client instead of cpanm to take advantage of mirror (if there is one in place)
-    cpan -i Module::Version Bundle::CPAN Log::Log4perl Module::Pluggable
-    # this guy below here will fail... but it's required although it's use is optional
-    cpan -T POE::Component::SSLify
-    cpan -i POE::Component::Metabase::Client::Submit POE::Component::Metabase::Relay::Server metabase::relayd CPAN::Reporter::Smoker::OpenBSD
 
     if [ ${USE_LOCAL_MIRROR} == 'true' ]
     then
@@ -67,14 +69,14 @@ then
         echo "remote: ${CPAN_MIRROR}" >> "${minicpanrc}"
         echo 'also_mirror: indices/find-ls.gz' >> "${minicpanrc}"
         (echo "o conf urllist file:///minicpan ${CPAN_MIRROR}"; echo 'o conf commit') | cpan
-        cpan -i CPAN::Mini CPAN::Mini::LatestDistVersion 
+        cpan -i CPAN::Mini CPAN::Mini::LatestDistVersion
         echo 'source ~/.bashrc' >> ~/.bash_profile
         echo "alias minicpan='minicpan -c CPAN::Mini::LatestDistVersion'" >> ~/.bashrc
         alias minicpan='minicpan -c CPAN::Mini::LatestDistVersion'
     fi
 
     cleanup_cpan
-    touch "${idempotent_control}"    
+    touch "${idempotent_control}"
     echo "Finished"
 fi
 
