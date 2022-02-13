1a0cc9f7ec4a6bf57a3f8d55680fe813bd40c941
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-02-13 19:23:12 -0300

    refactor: MFS defined by param
    Also added MFS configuration.

--- vagrant/scripts/fix_fstab.pl
+++ vagrant/scripts/fix_fstab.pl
@@ -1,17 +1,26 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use Getopt::Std;
+
+our %opts;
+getopts('b:', \%opts);
+
+die '-b is required' unless ((exists($opts{b})) and ($opts{b} ne ''));
+mkdir $opts{b} or die "Could not create $opts{b}: $!\n";
+chmod 0777, $opts{b};
 
 my $fstab = '/etc/fstab';
-
 open( my $in, '<', $fstab ) or die "Cannot read $fstab: $!\n";
 my @data = <$in>;
 close($in);
-
+my $has_mfs = 0;
 open( my $out, '>', $fstab ) or die "Cannot write to $fstab: $!\n";
+
 for my $line (@data) {
     chomp($line);
     my @parts = split( /\s/, $line );
+    $has_mfs = 1 if (($parts[1] eq $opts{b}) and ($parts[2] eq 'mfs' ));
 
     if (   ( $parts[1] eq '/home' )
         or ( $parts[1] eq '/minicpan' )
@@ -20,6 +29,11 @@ for my $line (@data) {
         $parts[3] = "$parts[3],softdep,noatime";
     }
 
+    print $out join( ' ', @parts ), "\n";
+}
+
+unless ($has_mfs) {
+    my @parts = ('swap', $opts{b}, 'mfs', 'rw,async,nodev,nosuid,-s=512m', '0', '0');
     print $out join( ' ', @parts ), "\n";
 }
 
