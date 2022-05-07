5c498fa4d0315de6633024a7483fb73f87a2f1c3
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-05-07 00:48:02 -0300

    fix: distroprefs undef warning

--- vagrant/packer/mariadb-user.sql
+++ vagrant/packer/mariadb-user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'vagrant'@'localhost';
+CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';
 GRANT ALL PRIVILEGES ON test.* TO 'vagrant'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'vagrant'@'localhost';
